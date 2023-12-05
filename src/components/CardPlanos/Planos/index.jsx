b96a7ab2f8874ea3df3e9f3d5d1db71706b7d6b4
Theo Furtado <58632127+theofurtado05@users.noreply.github.com>
2023-12-05 19:05:20 -0300

    .

--- src/components/CardPlanos/Planos/index.jsx
+++ src/components/CardPlanos/Planos/index.jsx
@@ -17,9 +17,9 @@ const Planos = () => {
                     <h1>Planos</h1>
                     <DivForm>
                         
-                        <CardPlano tituloPlano={'Dieta'} frequencia={'1 dieta'} valorPlano={'9,90'} numDietas={'1'} link={`../pixpage`}   adicionalAnual={false} avulso={true} linkPagamento={`https://pay.kiwify.com.br/PHko4N7?utm_content=${localStorage.getItem('@UserId:Nutrafity')}&src=1`}/>
-                        <CardPlano tituloPlano={'Dieta'} frequencia={'3 dietas'} valorPlano={'19,90'} numDietas={'3'} link={`../pixpage`}   adicionalAnual={true} avulso={true} linkPagamento={`https://pay.kiwify.com.br/niYZqRH?utm_content=${localStorage.getItem('@UserId:Nutrafity')}&src=3`}/>
-                        <CardPlano tituloPlano={'Dieta'} frequencia={'5 dietas'} valorPlano={'34,90'} numDietas={'5'} link={'../pixpage'}   adicionalAnual={false} avulso={true} linkPagamento={`https://pay.kiwify.com.br/7miepJ7?utm_content=${localStorage.getItem('@UserId:Nutrafity')}&src=5`}/>
+                        <CardPlano tituloPlano={'Dieta'} frequencia={'1 dieta'} valorPlano={'9,90'} numDietas={'1'} link={`../pixpage`}   adicionalAnual={false} avulso={true} linkPagamento={`https://pay.kiwify.com.br/SM2RRp3?utm_content=${localStorage.getItem('@UserId:Nutrafity')}&src=1`}/>
+                        <CardPlano tituloPlano={'Dieta'} frequencia={'3 dietas'} valorPlano={'19,90'} numDietas={'3'} link={`../pixpage`}   adicionalAnual={true} avulso={true} linkPagamento={`https://pay.kiwify.com.br/6NI7Emh?utm_content=${localStorage.getItem('@UserId:Nutrafity')}&src=3`}/>
+                        <CardPlano tituloPlano={'Dieta'} frequencia={'5 dietas'} valorPlano={'34,90'} numDietas={'5'} link={'../pixpage'}   adicionalAnual={false} avulso={true} linkPagamento={`https://pay.kiwify.com.br/iZumFDy?utm_content=${localStorage.getItem('@UserId:Nutrafity')}&src=5`}/>
 
                     </DivForm>
                 </DivFormPai>
