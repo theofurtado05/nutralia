b1b742a18c3297a2e24da0a904fa3e4bea4d52e8
Theo Furtado <58632127+theofurtado05@users.noreply.github.com>
2024-01-03 18:48:43 -0300

    .

--- src/services/verificaNavegador.js
+++ src/services/verificaNavegador.js
@@ -1,15 +1,5 @@
 export const VerificaNavegador = () => {
     const navegador = navigator
 
-    return {
-        appCodeName: navigator.appCodeName,
-        appName: navigator.appName,
-        appVersion: navigator.appVersion,
-        cookieEnabled: navigator.cookieEnabled,
-        language: navigator.language,
-        platform: navigator.platform,
-        userAgent: navigator.userAgent,
-        vendor: navigator.vendor,
-        userAgentData: navigator.userAgentData,
-    }
+    return navegador
 }
