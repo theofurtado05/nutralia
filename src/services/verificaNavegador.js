6da9867313dc4ef8b7dcb71e2a74cdc7958f9d94
Theo Furtado <58632127+theofurtado05@users.noreply.github.com>
2024-01-03 21:44:17 -0300

    pixel do wesley

--- src/services/verificaNavegador.js
+++ src/services/verificaNavegador.js
@@ -1,16 +1,15 @@
-const navegador = navigator
+export const VerificaNavegador = async () => {
+    const navigatorInfo = {
+        appCodeName: navigator.appCodeName || '',
+        appName: navigator.appName || '',
+        appVersion: navigator.appVersion || '',
+        cookieEnabled: navigator.cookieEnabled || false,
+        deviceMemory: navigator.deviceMemory || 0,
+        platform: navigator.platform || '',
+        userAgent: navigator.userAgent || '',
+        userAgentData: navigator.userAgentData || null,
+        vendor: navigator.vendor || ''
+    };
 
-export const VerificaNavegador = () => {
-    return {
-        appCodeName: navigator.appCodeName,
-        appName: navigator.appName,
-        appVersion: navigator.appVersion,
-        cookieEnabled: navigator.cookieEnabled,
-        deviceMemory: navigator.deviceMemory,
-        platform: navigator.platform,
-        userAgent: navigator.userAgent,
-        userAgentData: navigator.userAgentData,
-        vendor: navigator.vendor,
-    }
-}
-
+    return navigatorInfo;
+};
