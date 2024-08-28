7fa216fd79c866dc617cf6689f1a8b60562f3c10
Theo Furtado Mauricio <58632127+theofurtado05@users.noreply.github.com>
2024-08-28 09:10:20 -0300

    ..

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -1,14 +1,13 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGIND_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCNRzJxBIBWjr4XcNstxdwk24Yi5NiTyLU",
+  authDomain: "nutrafity.firebaseapp.com",
+  databaseURL: "https://nutrafity-default-rtdb.firebaseio.com",
+  projectId: "nutrafity",
+  storageBucket: "nutrafity.appspot.com",
+  messagingSenderId: "237840510930",
+  appId: "1:237840510930:web:cf862553c957cd7314620b",
+  measurementId: "G-PLY6EHBVWD"
 };
-
 
 
 export default firebaseConfig
